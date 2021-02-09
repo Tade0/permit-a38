c08fa9226345b6eb480ffe7243b05e976cf9208d
Tadeusz Piskozub <git@wringing.it>
2021-02-09 01:56:27 +0100

    Add no-async-promise-executor tests.

---
+++ test/eslint/no-async-promise-executor.test.js
@@ -0,0 +1,47 @@
+const {checkRuleForSource} = require('../../lib');
+const test = require('ava');
+
+const noAsyncPromiseExecutor = {
+  select: '$..[?(@.kind===#NewExpression# && @.expression.escapedText==="Promise")]',
+  filter: '$[*].arguments[0].modifiers[?(@.kind===#AsyncKeyword#)]',
+  matchType: 'none',
+  type: 'single-select-filter'
+};
+
+test('no-async-promise-executor bad', t => {
+  const source = `
+  const foo = new Promise(async (resolve, reject) => {
+    readFile('foo.txt', function(err, result) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+
+  const result = new Promise(async (resolve, reject) => {
+    resolve(await foo);
+  });
+  `;
+
+  t.is(checkRuleForSource(source, noAsyncPromiseExecutor), false);
+});
+
+test('no-async-promise-executor good', t => {
+  const source = `
+  const foo = new Promise((resolve, reject) => {
+    readFile('foo.txt', function(err, result) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+
+  const result = Promise.resolve(foo);
+  `;
+
+  t.is(checkRuleForSource(source, noAsyncPromiseExecutor), true);
+});
